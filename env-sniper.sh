ba70b115766f07cf3b01f947f05e4f31a12bf15b
Agung Mulya <agung@dolphin.sc.cc.tohoku.ac.jp>
2019-08-13 18:15:18 +0900

    Updated configs

--- env-sniper.sh
+++ env-sniper.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
+HOMEDIR=/home/agung
 #SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
-SCRIPT_DIR=$HOME/git-repo/sniper-detloc
+SCRIPT_DIR=$HOMEDIR/git-repo/sniper-detloc
 
 export GRAPHITE_ROOT=$SCRIPT_DIR
 echo "Set sniper root to ${GRAPHITE_ROOT}"
 
-export BENCHMARKS_ROOT=$(pwd)
+export BENCHMARKS_ROOT=$SCRIPT_DIR/benchmarks
 echo "Benchmark root: ${BENCHMARKS_ROOT}"
 
 
